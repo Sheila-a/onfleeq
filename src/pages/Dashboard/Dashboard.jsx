7bbdcd07b99a4716f6fb441436d529f97f1a3846
Sheila-a <sheila.codes.m@gmail.com>
2024-05-12 14:05:10 +0100

    feat: Laptop done, mobile 60%

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -21,8 +21,10 @@ import TopCard2 from '../../components/TopCard2';
 
 const Dashboard = () => {
   const [isLeftSelected, setIsLeftSelected] = useState(false);
+  const [isMobileOn, setisMobileOn] = useState(false);
 
   const toggleSwitch = () => {
+    setisMobileOn(true);
     setIsLeftSelected(!isLeftSelected);
   };
 
@@ -34,119 +36,156 @@ const Dashboard = () => {
             <SideNav />
           </div>
           <div
-            className={`w-[100%] relative p-5 md:p-10 md:pr-14 ${
+            className={`w-[100%] relative  md:p-10 md:pr-14 ${
               !isLeftSelected ? 'bg-white ' : 'bg-[#f6f8fe]'
-            } rounded-3xl z-10 `}
+            } rounded-3xl z-10  ${
+              isMobileOn == true &&
+              'r rounded-bl-[100px] rounded-br-[80px] min-h-[100vh]'
+            }`}
           >
-            <Header />
-            <div className='block md:hidden   '>
-              <div className=' flex justify-center items-center'>
-                <div className='rounded-lg w-[200px] h-[50px] shadow-2xl'>
-                  <div className='toggle '>
-                    <Icons
-                      icon='game-icons:network-bars'
-                      width={25}
-                      className={` left-[18%] ${
-                        isLeftSelected == false
-                          ? 'text-[#fff] bg-[#01c2f8]'
-                          : 'text-[#01c2f8] bg-white'
-                      } top-[22%] absolute z-50 cursor-pointer`}
-                    />
-                    <Icons
-                      icon='ph:credit-card-duotone'
-                      width={25}
-                      className={` left-[68%] ${
-                        isLeftSelected == false
-                          ? 'text-slate-300'
-                          : 'text-white'
-                      } top-[22%] absolute z-50 cursor-pointer`}
-                    />
-                    <input
-                      type='checkbox'
-                      name='toggle'
-                      id='toggle'
-                      className='shadow-2xl'
-                      checked={isLeftSelected}
-                      onChange={toggleSwitch}
-                    />
-                    <label htmlFor='toggle'></label>
-                  </div>
-                </div>
-              </div>{' '}
-              {!isLeftSelected && (
-                <div className='mt-14'>
-                  {' '}
-                  <TopCard />
-                </div>
-              )}
-              {isLeftSelected && 'hhh'}
-            </div>
-            <div className='md:grid hidden gap-7 lg:grid-cols-12'>
-              <div className=' lg:col-span-8'>
-                <div className=''>
-                  <TopCard2 />
-                </div>{' '}
-                <div className='fixed'>
+            {' '}
+            {isMobileOn && (
+              <div className='absolute bottom-0  w-full p-4 rounded-[50px] bg-[#3326ae]'>
+                <div className='flex relative justify-evenly'>
                   <Icons
                     icon='mingcute:home-3-fill'
                     width={35}
-                    className='text-[#fe382b] shadow-lg bg-white p-2 rounded-lg z-[999px] md:absolute hidden -left-[58px]  hover:text-white hover:bg-[#fe382b]  cursor-pointer'
+                    className='text-[#fe382b]   shadow-shede bg-white p-2 rounded-lg z-[999px] absolute left-5 -top-8  hover:text-white hover:bg-[#fe382b]  cursor-pointer'
+                  />{' '}
+                  <Icons
+                    icon='heroicons:chart-pie-20-solid'
+                    width={25}
+                    className='text-ongrey ml-14 hover:text-white cursor-pointer'
+                  />
+                  <Icons
+                    icon='ri:chat-smile-fill'
+                    hFlip='horizontal'
+                    width={25}
+                    className='text-ongrey hover:text-white cursor-pointer'
+                  />
+                  <Icons
+                    icon='ion:wallet'
+                    width={25}
+                    className='text-ongrey hover:text-white cursor-pointer'
                   />
                 </div>
-                <div className=' my-5 p-3 rounded-xl bg-ongrey2'>
-                  {/* <StatisticsChart /> */}
-                  <ApexChart />
-                </div>
-                <div className='flex gap-5 flex-col md:flex-row justify-between'>
-                  <div className='md:w-1/2 w-full'>
-                    <SpenTimeCard />
-                  </div>
-                  <div className='md:w-1/2 w-full'>
-                    <ConnectCard />
-                  </div>
-                </div>
               </div>
-              <div className='relative lg:col-span-4 p-6 bg-ongrey2 rounded-xl'>
-                <div className='flex justify-end text-boldgrey'>
-                  <Icons icon='iconamoon:edit-light' />
+            )}
+            <div className='p-5 md:p-0'>
+              <Header />
+              <div className='block md:hidden   '>
+                <div className=' flex justify-center items-center'>
+                  <div className='rounded-lg w-[200px] h-[50px] shadow-2xl'>
+                    <div className='toggle '>
+                      <Icons
+                        icon='game-icons:network-bars'
+                        width={25}
+                        className={` left-[18%] ${
+                          isLeftSelected == false
+                            ? 'text-[#fff] bg-[#01c2f8]'
+                            : 'text-[#01c2f8] bg-white'
+                        } top-[22%] absolute z-50 cursor-pointer`}
+                      />
+                      <Icons
+                        icon='ph:credit-card-duotone'
+                        width={25}
+                        className={` left-[68%] ${
+                          isLeftSelected == false
+                            ? 'text-slate-300'
+                            : 'text-white'
+                        } top-[22%] absolute z-50 cursor-pointer`}
+                      />
+                      <input
+                        type='checkbox'
+                        name='toggle'
+                        id='toggle'
+                        className='shadow-2xl'
+                        checked={isLeftSelected}
+                        onChange={toggleSwitch}
+                      />
+                      <label htmlFor='toggle'></label>
+                    </div>
+                  </div>
+                </div>{' '}
+                {!isLeftSelected && (
+                  <div className='mt-14'>
+                    {' '}
+                    <TopCard />
+                    <div className=' my-5 p-3 rounded-xl bg-ongrey2'>
+                      {/* <StatisticsChart /> */}
+                      <ApexChart />
+                    </div>
+                  </div>
+                )}
+                {isLeftSelected && 'hhh'}
+              </div>
+              <div className='md:grid hidden gap-7 lg:grid-cols-12'>
+                <div className=' lg:col-span-8'>
+                  <div className=''>
+                    <TopCard2 />
+                  </div>{' '}
+                  <div className='fixed'>
+                    <Icons
+                      icon='mingcute:home-3-fill'
+                      width={35}
+                      className='text-[#fe382b] shadow-lg bg-white p-2 rounded-lg z-[999px] md:absolute hidden -left-[58px]  hover:text-white hover:bg-[#fe382b]  cursor-pointer'
+                    />
+                  </div>
+                  <div className=' my-5 p-3 rounded-xl bg-ongrey2'>
+                    {/* <StatisticsChart /> */}
+                    <ApexChart />
+                  </div>
+                  <div className='flex gap-5 flex-col md:flex-row justify-between'>
+                    <div className='md:w-1/2 w-full'>
+                      <SpenTimeCard />
+                    </div>
+                    <div className='md:w-1/2 w-full'>
+                      <ConnectCard />
+                    </div>
+                  </div>
                 </div>
-                <div className='absolute -right-7 top-14 shadow-lg p-2 px-4 rounded-lg bg-[#3326ae] text-white'>
-                  $10,600
-                </div>
-                <div className='flex mt-6 items-center justify-center'>
-                  <div className='bg-white rounded-full w-36 h-36 relative'>
-                    <div className='flex items-center justify-center h-36'>
-                      <div className='bg-white rounded-full w-24 shadow-md h-24'>
-                        <div className='h-24 flex items-center justify-center'>
-                          <p className='text-onblue font-bold text-2xl'>
-                            38<span className='text-sm font-normal'>%</span>
-                          </p>
+                <div className='relative lg:col-span-4 p-6 bg-ongrey2 rounded-xl'>
+                  <div className='flex justify-end text-boldgrey'>
+                    <Icons icon='iconamoon:edit-light' />
+                  </div>
+                  <div className='absolute -right-7 top-14 shadow-lg p-2 px-4 rounded-lg bg-[#3326ae] text-white'>
+                    $10,600
+                  </div>
+                  <div className='flex mt-6 items-center justify-center'>
+                    <div className='bg-white rounded-full w-36 h-36 relative'>
+                      <div className='flex items-center justify-center h-36'>
+                        <div className='bg-white rounded-full w-24 shadow-md h-24'>
+                          <div className='h-24 flex items-center justify-center'>
+                            <p className='text-onblue font-bold text-2xl'>
+                              38<span className='text-sm font-normal'>%</span>
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className='h-36 w-36 absolute top-0'>
-                      {' '}
-                      <Lottie
-                        options={{
-                          animationData: deal,
-                          loop: false,
-                          autoplay: true,
-                        }}
-                        height={150}
-                        width={150}
-                      />
+                      <div className='h-36 w-36 absolute top-0'>
+                        {' '}
+                        <Lottie
+                          options={{
+                            animationData: deal,
+                            loop: false,
+                            autoplay: true,
+                          }}
+                          height={150}
+                          width={150}
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className='mt-9 '>
-                  <div className='flex justify-between items-center'>
-                    <h6 className='font-semibold text-slate-700'>My card</h6>
-                    <span className='t tracking-widest font-semibold text-slate-400'>
-                      ..
-                    </span>
+                  <div className='mt-9 '>
+                    <div className='flex justify-between items-center'>
+                      <h6 className='font-semibold text-slate-700'>My card</h6>
+                      <span className='t tracking-widest font-semibold text-slate-400'>
+                        ..
+                      </span>
+                    </div>
+                    <img src={card} className='mt-5 w-full mb-2' />
+                    <Transaction />
                   </div>
-                  <img src={card} className='mt-5 w-full mb-2' />
-                  <Transaction />
                 </div>
               </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ export default {
         ongrey2: '#f4f4fc',
         boldgrey: '#c5bcd5',
       },
+      boxShadow: {
+        shede:
+          ' box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;',
+      },
     },
   },
   plugins: [],
